d3efb0577bf7576e641dee51db4e66d9a7c554b6
Leandro Abreu <leandro.sabreu@uvvnet.com.br>
2022-05-14 14:45:54 -0300

    Criando Components e Rotas

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {AppComponent} from 'src/app/app.component'
+import { FornecedoresFormComponent } from './fornecedores/fornecedores-form/fornecedores-form/fornecedores-form.component';
+import { FornecedoresComponent } from './fornecedores/fornecedores/fornecedores.component';
+import { MenuComponent } from './menu/menu/menu.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: MenuComponent},
+  {path: 'cadastrarFornecedor', component: FornecedoresFormComponent},
+  {path: 'fornecedores', component: FornecedoresComponent},
+
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes),
-    RouterModule.forChild([
-			{path:'',component: AppComponent}
-		])],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -5,10 +5,16 @@ import {ButtonModule} from 'primeng/button';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FornecedoresComponent } from './fornecedores/fornecedores/fornecedores.component';
+import { FornecedoresFormComponent } from './fornecedores/fornecedores-form/fornecedores-form/fornecedores-form.component';
+import { MenuComponent } from './menu/menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FornecedoresComponent,
+    FornecedoresFormComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
